012cc2e1c1d5ba1cf12a94425cb919cf0de65ccf
SeinopSys <seinopsys@protonmail.com>
2026-02-28 17:52:14 +0100

    Implement Stock API endpoints

--- src/stock/stock.controller.ts
+++ src/stock/stock.controller.ts
@@ -1,17 +1,38 @@
-import { Controller, Get, Param, Put } from '@nestjs/common';
-import { StockDbalService } from './stock-dbal.service.js';
+import { Controller, Get, HttpException, Put } from '@nestjs/common';
+import { StockService } from './stock.service.js';
+
+export interface GetStockResponse {
+  price: number;
+  lastUpdatedAt: string;
+  movingAverage: number;
+}
+export interface PutStockResponse {
+  message: string;
+}
 
 @Controller('stock')
 export class StockController {
-  constructor(protected stockDbal: StockDbalService) {}
+  constructor(private readonly stockService: StockService) {}
 
-  @Get(':symbol')
-  async getCurrentStockPrice(@Param('symbol') symbol: string) {
-    return this.stockDbal.stock({ ticker: symbol });
+  @Get('{symbol}')
+  async getStock(symbol: string): Promise<GetStockResponse> {
+    const priceData = await this.stockService.getStockPriceData(symbol);
+    if (!priceData) {
+      throw new HttpException('No price data found', 404);
+    }
+    const movingAverage = await this.stockService.getMovingAverage(symbol);
+    return {
+      price: priceData.price,
+      lastUpdatedAt: priceData.createdAt.toISOString(),
+      movingAverage,
+    };
   }
 
-  @Put(':symbol')
-  async putCurrentStockPrice(@Param('symbol') symbol: string) {
-    return this.stockDbal.createStock({ ticker: symbol, price: 0 });
+  @Put('{symbol}')
+  putStock(symbol: string): PutStockResponse {
+    const scheduled = this.stockService.schedulePriceUpdates(symbol);
+    return {
+      message: `Stock price updates ${scheduled ? 'have been scheduled' : 'are already scheduled'}`,
+    };
   }
 }
